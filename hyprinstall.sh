47d0469c512ff362a41c71f3094ed0b24064414a
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-11-16 20:21:01 +0000

    Update hyprinstall.sh

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -1,4 +1,5 @@
-yay -S hyprland kitty thunar mako wofi waybar-hyprland hyprpaper mpv grim slurp swappy wl-clipboard cliphist gvfs plymouth \
-pipewire wireplumber pavucontrol xdg-desktop-portal-hyprland xdg-user-dirs lxappearance polkit-gnome file-roller brightnessctl \
-bluez bluez-utils blueman network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts noto-fonts ttf-font-awesome \
-noto-fonts-emoji grimblast-git wlogout swaylock-effects avizo sddm-git sddm-sugar-candy-git xfce4-settings ttf-jetbrains-mono-nerd
+yay -S hyprland kitty thunar firefox mako wofi waybar-hyprland hyprpaper mpv grim slurp swappy wl-clipboard cliphist gvfs jq gcc\
+pipewire wireplumber pavucontrol xdg-desktop-portal-hyprland xdg-user-dirs lxappearance xfce4-settings polkit-gnome qt5-wayland \
+qt6-wayland qt5ct qt6ct qt5‑graphicaleffects qt5‑quickcontrols2 file-roller brightnessctl bluez bluez-utils blueman python-requests \
+network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts noto-fonts ttf-font-awesome ttf-jetbrains-mono-nerd \
+noto-fonts-emoji grimblast-git wlogout swaylock-effects avizo sddm-git sddm-sugar-candy-git papirus-icon-theme plymouth
