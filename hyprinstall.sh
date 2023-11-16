1cdd094ee73be2ee8814f0c2523e45c926ee5440
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-11-16 19:10:27 +0000

    Create hyprinstall.sh

---
+++ hyprinstall.sh
@@ -0,0 +1,4 @@
+yay -S hyprland kitty thunar mako wofi waybar-hyprland hyprpaper mpv grim slurp swappy wl-clipboard cliphist gvfs plymouth \
+pipewire wireplumber pavucontrol xdg-desktop-portal-hyprland xdg-user-dirs lxappearance polkit-gnome file-roller brightnessctl \
+bluez bluez-utils blueman network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts noto-fonts ttf-font-awesome \
+noto-fonts-emoji grimblast-git wlogout swaylock-effects avizo sddm-git sddm-sugar-candy-git xfce4-settings ttf-jetbrains-mono-nerd
