3b49223e59d1ab58bf9c5a42ba044354355e599a
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-11-17 16:27:45 +0000

    Update and rename hyprinstall.sh to install_hypr.sh

--- install_hypr.sh
+++ install_hypr.sh
@@ -1,6 +1,8 @@
-yay -S hyprland kitty vim thunar firefox thunderbird discord mako wofi waybar wlogout swww avizo mpv grim slurp swappy gvfs plymouth \
+yay -S --needed --noconfirm hyprland kitty vim thunar firefox thunderbird discord mako wofi waybar wlogout swww avizo mpv grim slurp swappy gvfs plymouth \
 jq gcc wl-clipboard cliphist pipewire wireplumber pavucontrol pamixer wf-recorder gedit xdg-desktop-portal-hyprland xdg-user-dirs \
 lxappearance xfce4-settings polkit-gnome qt5-wayland qt6-wayland qt5ct qt6ct qt5‑graphicaleffects qt5‑quickcontrols2 file-roller \
 brightnessctl bluez bluez-utils blueman python-requests network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts \
 noto-fonts noto-fonts-emoji ttf-font-awesome ttf-jetbrains-mono-nerd grimblast-git swaylock-effects sddm-git sddm-sugar-candy-git \
 papirus-icon-theme
+cp ~/hypr-dots/.config ~/.config
+cp ~/hypr-dots/wallpapers ~
