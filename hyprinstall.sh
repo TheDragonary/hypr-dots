cdeab0986bbdfef3e2d46ed443cf3646cacb31d5
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-11-17 00:24:35 +0000

    Update hyprinstall.sh

--- hyprinstall.sh
+++ hyprinstall.sh
@@ -1,6 +1,6 @@
-yay -S hyprland kitty thunar firefox thunderbird discord mako wofi waybar-hyprland hyprpaper mpv grim slurp swappy gvfs jq gcc \
-wl-clipboard cliphist pipewire wireplumber pavucontrol xdg-desktop-portal-hyprland xdg-user-dirs lxappearance xfce4-settings \
-polkit-gnome qt5-wayland qt6-wayland qt5ct qt6ct qt5‑graphicaleffects qt5‑quickcontrols2 file-roller brightnessctl bluez \
-bluez-utils blueman python-requests network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts noto-fonts \
-ttf-font-awesome ttf-jetbrains-mono-nerd noto-fonts-emoji grimblast-git wlogout swaylock-effects avizo sddm-git \
-sddm-sugar-candy-git papirus-icon-theme plymouth wf-recorder
+yay -S hyprland kitty vim thunar firefox thunderbird discord mako wofi waybar-hyprland hyprpaper mpv grim slurp swappy gvfs \
+jq gcc wl-clipboard cliphist pipewire wireplumber pavucontrol wf-recorder gedit xdg-desktop-portal-hyprland xdg-user-dirs plymouth \
+lxappearance xfce4-settings polkit-gnome qt5-wayland qt6-wayland qt5ct qt6ct qt5‑graphicaleffects qt5‑quickcontrols2 file-roller \
+brightnessctl bluez bluez-utils blueman python-requests network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts \
+noto-fonts noto-fonts-emoji ttf-font-awesome ttf-jetbrains-mono-nerd grimblast-git wlogout swaylock-effects avizo sddm-git \
+sddm-sugar-candy-git papirus-icon-theme
