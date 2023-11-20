2f8f1a985194371dda994b86442ae83bea1f0683
TheDragonary <ibrahimzaman2004@gmail.com>
2023-11-20 16:48:59 +0000

    Update configs and add start_sddm.sh

--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo systemctl enable sddm.service && sudo systemctl start sddm.service
+sudo systemctl enable sddm.service
 
 echo -e "[Theme]\nCurrent=sugar-candy\nCursorTheme=Bibata-Modern-Classic\nCursorSize=24" | sudo tee /etc/sddm.conf
