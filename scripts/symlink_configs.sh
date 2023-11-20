37eb08eb53a513b58fc3441ac57de87365931a09
TheDragonary <ibrahimzaman2004@gmail.com>
2023-11-20 00:40:49 +0000

    Update configs

---
+++ scripts/symlink_configs.sh
@@ -0,0 +1,6 @@
+ln -s ~/hypr-dots/.config/hypr ~/.config/hypr
+ln  -s ~/hypr-dots/.config/swaylock ~/.config/swaylock
+ln -s ~/hypr-dots/.config/waybar ~/.config/waybar
+ln -s ~/hypr-dots/.config/wlogout ~/.config/wlogout
+ln -s ~/hypr-dots/.config/swww.sh ~/.config/swww.sh
+ln -s ~/hypr-dots/wallpapers ~/wallpapers
