744de8f3a932263e863027e209ed8404e67649d0
TheDragonary <ibrahimzaman2004@gmail.com>
2023-11-21 01:29:53 +0000

    Update install_hypr.sh

--- scripts/install_hypr.sh
+++ scripts/install_hypr.sh
@@ -1,7 +1,7 @@
-yay -S --needed --noconfirm hyprland kitty vim thunar firefox thunderbird discord mako wofi waybar wlogout swww avizo mpv \
+yay -S --needed --noconfirm hyprland kitty vim thunar firefox thunderbird discord mako wofi waybar wlogout swww mpv \
 grim slurp swappy gvfs plymouth jq gcc wl-clipboard cliphist pipewire wireplumber pavucontrol pamixer wf-recorder gedit \
 xdg-desktop-portal-hyprland xdg-user-dirs lxappearance xfce4-settings polkit-gnome qt5-wayland qt6-wayland qt5ct qt6ct \
 qt5‑graphicaleffects qt5‑quickcontrols2 file-roller brightnessctl bluez bluez-utils blueman htop vtop nwg-look-bin pywal \
 network-manager-applet thunar-archive-plugin pacman-contrib gnu-free-fonts noto-fonts noto-fonts-emoji ttf-font-awesome \
 ttf-jetbrains-mono-nerd grimblast-git swaylock-effects sddm-git sddm-sugar-candy-git papirus-icon-theme bibata-cursor-theme \
-ttf-fira-sans ttf-firacode-nerd fd swayidle tumbler ffmpegthumbnailer pfetch cava tty-clock code spotify
+ttf-fira-sans ttf-firacode-nerd fd swayidle swayosd-git tumbler ffmpegthumbnailer pfetch cava tty-clock code spotify
