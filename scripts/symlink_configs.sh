579b680d242095d18e34999b0b7522f2c2cab8a1
TheDragonary <ibrahimzaman2004@gmail.com>
2023-11-23 00:45:06 +0000

    Update hyprland.conf, update scripts, add mako and ranger configs

--- scripts/symlink_configs.sh
+++ scripts/symlink_configs.sh
@@ -3,15 +3,21 @@
 # Delete old folders/symlinks
 rm -rf ~/.config/hypr
 rm -rf ~/.config/swaylock
+rm -rf ~/.config/swayidle
 rm -rf ~/.config/waybar
 rm -rf ~/.config/wlogout
 rm -rf ~/.config/swww
+rm -rf ~/.config/mako
+rm -rf ~/.config/ranger
 rm -rf ~/wallpapers
 
 # Create symbolic links
-ln -s ~/hypr-dots/.config/hypr ~/.config/hypr
-ln  -s ~/hypr-dots/.config/swaylock ~/.config/swaylock
-ln -s ~/hypr-dots/.config/waybar ~/.config/waybar
-ln -s ~/hypr-dots/.config/wlogout ~/.config/wlogout
-ln -s ~/hypr-dots/.config/swww ~/.config/swww
-ln -s ~/hypr-dots/wallpapers ~/wallpapers
+ln -s ~/hypr-dots/.config/hypr ~/.config
+ln -s ~/hypr-dots/.config/swaylock ~/.config
+ln -s ~/hypr-dots/.config/swayidle ~/.config
+ln -s ~/hypr-dots/.config/waybar ~/.config
+ln -s ~/hypr-dots/.config/wlogout ~/.config
+ln -s ~/hypr-dots/.config/swww ~/.config
+ln -s ~/hypr-dots/.config/mako ~/.config
+ln -s ~/hypr-dots/.config/ranger ~/.config
+ln -s ~/hypr-dots/wallpapers ~
