5ca82e035d8a0aec0473efb54e24bc1612670dc7
TheDragonary <ibrahimzaman2004@gmail.com>
2024-03-18 13:29:46 +0000

    Update .bashrc and fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go her
+    # Commands to run in interactive sessions can go here
     starship init fish | source
     pfetch
     set fish_greeting
     alias hyprconf="vim .config/hypr/hyprland.conf"
+    set -gx MANPAGER nvim +Man!
 end
